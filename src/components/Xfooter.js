cee465ae34206ff52359980bdfe7dd70b9c2afd3
ll <304566206@qq.com>
2019-04-01 11:27:47 +0800

    底部基本功能实现 缺购物车数量完善

--- src/components/Xfooter.js
+++ src/components/Xfooter.js
@@ -4,6 +4,8 @@ import '../../style/Xfooter.css';
 
 import {withRouter} from 'react-router-dom'
 
+import {Badge} from 'antd'
+
 class Xfooter extends React.Component {
     constructor() {
         super();
@@ -13,58 +15,83 @@ class Xfooter extends React.Component {
                     title: "首页",
                     name: "home",
                     path: "/",
-                    icon : 'icon icon_homenew_on'
+                    icon: 'icon icon_homenew_on'
                 },
                 {
                     title: "国家馆",
                     name: "nation",
                     path: "/nation",
-                    icon : 'icon icon_country2'
+                    icon: 'icon icon_country2'
                 },
                 {
                     title: "分类",
                     name: "category",
                     path: "/category",
-                    icon : 'icon icon_classification'
+                    icon: 'icon icon_classification'
                 },
                 {
                     title: "购物车",
                     name: "cart",
                     path: "/cart",
-                    icon : 'icon icon_Cart'
+                    icon: 'icon icon_Cart'
                 },
                 {
                     title: "我",
                     name: "mine",
                     path: "/mine",
-                    icon : 'icon icon_me_unselected'
+                    icon: 'icon icon_me_unselected'
                 }
-            ]
+            ],
+            cur: 'home'
         }
     }
 
-    goto(path){
+    goto(path, name) {
         let {history} = this.props;
         history.push({
-            pathname : path
+            pathname: path
+        })
+        this.setState({
+            cur: name
         })
     }
 
     render() {
-        let {navlist} = this.state
+        let {navlist, cur} = this.state
         return <section className="foot_tab clearfix">
             <ul className="clearfix">
-                    {
-                        navlist.map(item=>(
-                            <li key={item.name} onClick={this.goto.bind(this,item.path)}>
-                                <div className="link ">
-                                    <i className={item.icon}>
-                                    </i>
-                                    <div>{item.title}</div>
-                                </div>
-                            </li>
-                        ))
-                    }
+                {
+                    navlist.map(item => (
+                        <li key={item.name} onClick={this.goto.bind(this, item.path, item.name)}
+                            style={cur === item.name ? {color: '#00bebf'} : {}}>
+
+                                {
+                                    item.name === 'cart' ? (<Badge count={5}>
+                                                                <div className="link ">
+                                                                <i className={item.icon}>
+                                                                </i>
+                                                                <div>{item.title}</div>
+                                                                </div>
+                                                            </Badge>)
+                                        :
+                                        (
+
+                                                <div className="link">
+                                                    <i className={item.icon}>
+                                                    </i>
+                                                    <div>
+                                                        {item.title}
+                                                    </div>
+                                                </div>
+                                        )
+
+                                }
+
+
+
+                        </li>
+                    ))
+                }
             </ul>
         </section>
     }
